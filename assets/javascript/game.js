f3c26f56c3754f9022a768b7f87bc724ee7b646d
jarrenpatao <jarrenpatao@gmail.com>
2018-11-16 16:11:08 -0800

    appended question to div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,93 @@
-// Trivia Questions & Answers as Objects
-var tq1 = { 
-  ques: "How long did the Hundred Years' War last?",
-  ans0: "1.) 98",
-  ans1: "2.) 100",
-  ans2: "3.) 110",
-  ans3: "4.) 116"
-  // ans3
+var questions = {
+  questionOne: {
+    queString: "How long did the Hundred Years' War last?",
+    answerOne: "1.) 98",
+    answerTwo: "2.) 100",
+    answerThree: "3.) 110",
+    answerFour: "4.) 116"
+    // answer is 3.
+  },
+  questionTwo: {
+    queString: "How many months of the year have 28 days?",
+    answerOne: "1.) 1",
+    answerTwo: "2.) 3 every 4 years",
+    answerThree: "3.) 17",
+    answerFour: "4.) 12"
+    // answer is 3.
+  },
+  questionThree: {
+    queString: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+    answerOne: "1.) William and Elizabeth",
+    answerTwo: "2.) Joseph and Catherine",
+    answerThree: "3.) John and Mary",
+    answerFour: "4.) George and Anne"
+    // ans2
+  },
+  questionFour: {
+    queString: "When did the Liberty Bell get its name?",
+    answerOne: "1.) When it was made, in 1701",
+    answerTwo: "2.) When it rang on July 4, 1776",
+    answerThree: "3.) In the 19th century, when it became a symbol of the abolition of slavery",
+    answerFour: "4.) None of the above"
+    // ans2
+  },
+  questionFive: {
+    queString: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
+    answerOne: "1.) Buttermilk",
+    answerTwo: "2.) Daisy",
+    answerThree: "3.) Scout",
+    answerFour: "4.) Tulip"
+    // answerOne
+  },
+  questionSix: {
+    queString: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+    answerOne: "1.) Home computer",
+    answerTwo: "2.) Compact disk player",
+    answerThree: "3.) Cordless phone",
+    answerFour: "4.) Dishwasher"
+    // answerTwo
+  },
+  questionSeven: {
+    queString: "Who holds the record for the most victories in a row on the professional golf tour?",
+    answerOne: "1.) Jack Nicklaus",
+    answerTwo: "2.) Arnold Palmer",
+    answerThree: "3.) Byron Nelson",
+    answerFour: "4.) Ben Hogan"
+    // answerThree
+  },
+  questionEight: {
+    queString: "Who is third behind Hank Aaron and Babe Ruth in major league career home runs?",
+    answerOne: "1.) Reggie Jackson",
+    answerTwo: "2.) Harmon Killebrew",
+    answerThree: "3.) Willie Mays",
+    answerFour: "4.) Frank Robinson"
+    // answerThree
+  },
+  questionNine: {
+    queString: "In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?",
+    answerOne: "1.) 8",
+    answerTwo: "2.) 18",
+    answerThree: "3.) 38",
+    answerFour: "4.) 58"
+    // answerTwo
+  },
+  questionTen: {
+    queString: "The Philadelphia mint started putting a 'P' mint mark on quarters when?",
+    answerOne: "1.) 1960",
+    answerTwo: "2.) 1980",
+    answerThree: "3.) Never",
+    answerFour: "4.) All of the above"
+    // answerTwo
+  },
+  questionEleven: {
+    queString: "Which of these characters turned 40 years old in 1990?",
+    answerOne: "1.) Charlie Brown",
+    answerTwo: "2.) Bugs Bunny",
+    answerThree: "3.) Mickey Mouse",
+    answerFour: "4.) Fred Flinstone"
+    // answerThree
+  }
 }
-var tq2 = {
-  ques: "How many months of the year have 28 days?",
-  ans0: "1.) 1",
-  ans1: "2.) 3 every 4 years",
-  ans2: "3.) 17",
-  ans3: "4.) 12"
-  // ans3
-}
-var tq3 = {
-  ques: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
-  ans0: "1.) William and Elizabeth",
-  ans1: "2.) Joseph and Catherine",
-  ans2: "3.) John and Mary",
-  ans3: "4.) George and Anne"
-  // ans2
-}
-var tq4 = {
-  ques: "When did the Liberty Bell get its name?",
-  ans0: "1.) When it was made, in 1701",
-  ans1: "2.) When it rang on July 4, 1776",
-  ans2: "3.) In the 19th century, when it became a symbol of the abolition of slavery",
-  ans3: "4.) None of the above"
-  // ans2
-}
-var tq5 = {
-  ques: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
-  ans0: "1.) Buttermilk",
-  ans1: "2.) Daisy",
-  ans2: "3.) Scout",
-  ans3: "4.) Tulip"
-  // ans0
-}
-var tq6 = {
-  ques: "Which of the following items was owned by the fewest U.S. homes in 1990?",
-  ans0: "1.) Home computer",
-  ans1: "2.) Compact disk player",
-  ans2: "3.) Cordless phone",
-  ans3: "4.) Dishwasher"
-  // ans1
-}
-var tq7 = {
-  ques: "Who holds the record for the most victories in a row on the professional golf tour?",
-  ans0: "1.) Jack Nicklaus",
-  ans1: "2.) Arnold Palmer",
-  ans2: "3.) Byron Nelson",
-  ans3: "4.) Ben Hogan"
-  // ans2
-}
-var tq8 = {
-  ques: "Who is third behind Hank Aaron and Babe Ruth in major league career home runs?",
-  ans0: "1.) Reggie Jackson",
-  ans1: "2.) Harmon Killebrew",
-  ans2: "3.) Willie Mays",
-  ans3: "4.) Frank Robinson"
-  // ans2
-}
-var tq9 = {
-  ques: "In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?",
-  ans0: "1.) 8",
-  ans1: "2.) 18",
-  ans2: "3.) 38",
-  ans3: "4.) 58"
-  // ans1
-} 
-var tq10 = {
-  ques: "The Philadelphia mint started putting a 'P' mint mark on quarters when?",
-  ans0: "1.) 1960",
-  ans1: "2.) 1980",
-  ans2: "3.) Never",
-  ans3: "4.) All of the above"
-  // ans1
-} 
-var tq11 = {
-  ques: "Which of these characters turned 40 years old in 1990?",
-  ans0: "1.) Charlie Brown",
-  ans1: "2.) Bugs Bunny",
-  ans2: "3.) Mickey Mouse",
-  ans3: "4.) Fred Flinstone"
-  // ans2
-} 
-
-// All Trivia Questions
-var atq = [tq1,tq2,tq3,tq4,tq5,tq6,tq7,tq8,tq9,tq10,tq11];
 // CountDown Number. Placed at global level to recall after answerSelected
 var icdn = 4;
 // Round CountDown
@@ -159,9 +157,8 @@ icdf();
 
 // Loop through questions
 if (!gameStart)
-  for (var i = 0; i < atq.length; i++){
-    var questi = $('');
-    $('#qr').append(questi);
-
+  for (var i = 0; i < questions.length; i++){
+    $('#qr').append('<h2>' + questions.questionOne.queString + '</h2>');
+    $('#qa').append(questions.questionOne.answerOne);
   }
-  console.log();
+  console.log(questions.questionOne.queString);
